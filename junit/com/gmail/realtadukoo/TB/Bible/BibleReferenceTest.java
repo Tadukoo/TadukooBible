5c5743fbe1216f4df0e5ab80a21619625754fdb2
Tadukoo <realtadukoo@gmail.com>
2019-05-21 15:10:08 -0400

    Added BibleReferenceTest and Finished MissingCommand

---
+++ junit/com/gmail/realtadukoo/TB/Bible/BibleReferenceTest.java
@@ -0,0 +1,61 @@
+package com.gmail.realtadukoo.TB.Bible;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class BibleReferenceTest{
+	private BibleReference ref;
+	
+	@Before
+	public void setup(){
+		ref = new BibleReference(EnumBible.JOHN1, 3, 16, EnumTranslations.ESV);
+	}
+	
+	@Test
+	public void testEmptyConstructor(){
+		BibleReference ref = new BibleReference();
+		
+		assertEquals(null, ref.getBook());
+		assertEquals(-1, ref.getChapter());
+		assertEquals(-1, ref.getVerse());
+		assertEquals(null, ref.getTranslation());
+	}
+	
+	@Test
+	public void testConstructor(){
+		assertEquals(EnumBible.JOHN1, ref.getBook());
+		assertEquals(3, ref.getChapter());
+		assertEquals(16, ref.getVerse());
+		assertEquals(EnumTranslations.ESV, ref.getTranslation());
+	}
+	
+	@Test
+	public void testSetBook(){
+		ref.setBook(EnumBible.ACTS);
+		
+		assertEquals(EnumBible.ACTS, ref.getBook());
+	}
+	
+	@Test
+	public void testSetChapter(){
+		ref.setChapter(28);
+		
+		assertEquals(28, ref.getChapter());
+	}
+	
+	@Test
+	public void testSetVerse(){
+		ref.setVerse(21);
+		
+		assertEquals(21, ref.getVerse());
+	}
+	
+	@Test
+	public void testSetTranslation(){
+		ref.setTranslation(EnumTranslations.HCSB);
+		
+		assertEquals(EnumTranslations.HCSB, ref.getTranslation());
+	}
+}
--- src/com/gmail/realtadukoo/TB/Command/MissingCommand.java
+++ src/com/gmail/realtadukoo/TB/Command/MissingCommand.java
@@ -1,6 +1,7 @@
 package com.gmail.realtadukoo.TB.Command;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 import com.gmail.realtadukoo.TB.FindMissing;
 import com.gmail.realtadukoo.TB.Bible.EnumTranslations;
@@ -8,13 +9,13 @@ import com.gmail.realtadukoo.TB.Bible.EnumTranslations;
 public class MissingCommand extends Command{
 	
 	public MissingCommand(){
-		// TODO: THIS
-		super("");
+		super("<Tran:translation>");
 	}
 	
 	@Override
 	public String[] runCommand(ArrayList<String> args){
-		EnumTranslations tran = ArgumentParsing.isTranslation(args);
+		HashMap<String, Object> objs = getArgsAsObjects(args);
+		EnumTranslations tran = (EnumTranslations) objs.get("Tran");
 		FindMissing.run(tran);
 		return new String[]{};
 	}
